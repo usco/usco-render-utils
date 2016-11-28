c7e07484742f355f17ed5654b070d7eda45bd175
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-28 12:25:03 +0100

    feat(exports): added exports of all sub modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export {default as render} from './render'
+export {default as prepareRender} from './render'
 export {default as wrapperScope} from './wrapperScope'
+
+export {default as drawCuboid} from './drawCuboid'
+export {default as drawCuboidFromCoords} from './drawCuboidFromCoords'
+export {default as drawDynMesh} from './drawDynMesh'
+export {default as drawGrid} from './drawGrid'
+export {default as drawImgPlane} from './drawImgPlane'
+export {default as drawStaticMesh} from './drawStaticMesh'
+export {default as drawStaticMesh2} from './drawStaticMesh2'
+export {default as drawTri} from './drawTri'
+
+// FIXME: how do we deal with shaders etc ? not es6 module syntax compatible at all ! (will never be)
 //export drawEnclosure from './drawEnclosure'
